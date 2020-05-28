bf5bbd411e68e14a6ea017598079a5776facd060
Joe <joe@joebailey.xyz>
2020-05-28 21:14:58 +0100

    Fixed spacing and semi-colon

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,46 +1,45 @@
-
 <style scoped>
   .calculator {
-		font-size: 32px;
-		width: fit-content;
-		display: grid;
-		grid-template-columns: 1fr 1fr 1fr 1fr;
-		grid-gap: .5em;
-		background-color: #C0C0C0;
-		padding: 1em;
-		border-radius: .5em .5em 1em 1em;
-		-moz-box-shadow: inset 0 0 5px 0 #8a8a8a;
-    -webkit-box-shadow: inset 0 0 5px 0 #8a8a8a;
-    box-shadow: inset 0 0 5px 0 #8a8a8a;
+		font-size: 32px
+		width: fit-content
+		display: grid
+		grid-template-columns: 1fr 1fr 1fr 1fr
+		grid-gap: .5em
+		background-color: #C0C0C0
+		padding: 1em
+		border-radius: .5em .5em 1em 1em
+		-moz-box-shadow: inset 0 0 5px 0 #8a8a8a
+    -webkit-box-shadow: inset 0 0 5px 0 #8a8a8a
+    box-shadow: inset 0 0 5px 0 #8a8a8a
 	}
 	.calculator--top {
-		display: grid;
-		grid-auto-flow: column;
-		grid-column: 1 / 5;
-		place-items: center;
-		margin-left: -.5em;
-		margin-right: -.5em;
+		display: grid
+		grid-auto-flow: column
+		grid-column: 1 / 5
+		place-items: center
+		margin-left: -.5em
+		margin-right: -.5em
 	}
 	.calculator--top p {
-		text-transform: uppercase;
-		color: #555;
-		font-family: sans-serif;
-		font-weight: 900;
-		margin: 0;
+		text-transform: uppercase
+		color: #555
+		font-family: sans-serif
+		font-weight: 900
+		margin: 0
 	}
 	.calculator--top_solar {
-		display: grid;
-		grid-template-columns: 1fr 1fr 1fr 1fr;
-		border: .15em solid grey;
-		border-radius: .25em;
-		height: 1.66em;
-		background-color: #364441;
-		grid-gap: .1em;
-		justify-self: end;
+		display: grid
+		grid-template-columns: 1fr 1fr 1fr 1fr
+		border: .15em solid grey
+		border-radius: .25em
+		height: 1.66em
+		background-color: #364441
+		grid-gap: .1em
+		justify-self: end
 	}
 	.solar {
-		background-color: #443232;
-		width: 40px;
+		background-color: #443232
+		width: 40px
 	}
 	.solar:first-of-type {
 		border-radius: .1em 0 0 .1em
@@ -49,54 +48,54 @@
 		border-radius: 0 .1em .1em 0
 	}
 	.calculator--screen {
-		grid-column: 1 / 5;
-		font-family: 'Calculator';
-		background-color: #006d59;
-		border: .5em solid grey;
-		border-radius: .25em .25em .5em .5em;
-		margin-left: -.5em;
-		margin-right: -.5em;
-		display: grid;
-		align-content: end;
-		height: 140px;
+		grid-column: 1 / 5
+		font-family: 'Calculator'
+		background-color: #006d59
+		border: .5em solid grey
+		border-radius: .25em .25em .5em .5em
+		margin-left: -.5em
+		margin-right: -.5em
+		display: grid
+		align-content: end
+		height: 140px
 	}
 	.calculator--screen_value {
-		display: grid;
-		justify-content: end;
-		padding: 0 16px;
-		padding-bottom: 8px;
-		font-size: 64px;
-		line-height: 64px;
+		display: grid
+		justify-content: end
+		padding: 0 16px
+		padding-bottom: 8px
+		font-size: 64px
+		line-height: 64px
 	}
 	.calculator--screen_calc {
-		display: grid;
-		justify-content: end;
-		padding-top: 8px;
-		padding-right: 16px;
-		font-size: 24px;
+		display: grid
+		justify-content: end
+		padding-top: 8px
+		padding-right: 16px
+		font-size: 24px
 	}
 	.calculator--button {
-		border-radius: .25em .25em .5em .5em;
-		border-color: #111;
-		background-color: #333;
-		color: white;
-		height: 2em;
-		width: 2.33em;
-		font-size: inherit;
-		line-height: 100%;
-		display: grid;
-		place-items: center;
-		font-family: 'Roboto Mono', monospace;
+		border-radius: .25em .25em .5em .5em
+		border-color: #111
+		background-color: #333
+		color: white
+		height: 2em
+		width: 2.33em
+		font-size: inherit
+		line-height: 100%
+		display: grid
+		place-items: center
+		font-family: 'Roboto Mono', monospace
 	}
 	.calculator--button:focus {
-		outline: 0;
-		-moz-box-shadow: inset 0 0 10px 0 #000000;
-		-webkit-box-shadow: inset 0 0 10px 0 #000000;
-		box-shadow: inset 0 0 10px 0 #000000;
+		outline: 0
+		-moz-box-shadow: inset 0 0 10px 0 #000000
+		-webkit-box-shadow: inset 0 0 10px 0 #000000
+		box-shadow: inset 0 0 10px 0 #000000
 	}
 	.add {
-		grid-row: span 2;
-		height: 100%;
+		grid-row: span 2
+		height: 100%
 	}
 </style>
 
@@ -192,58 +191,58 @@
     },
     methods: {
 		clear() {
-			this.current = '';
-			this.equation = [];
+			this.current = ''
+			this.equation = []
 		},
 		sign() {
 			if (this.current !== '') {
-				this.current = this.current.charAt(0) === '-' ? this.current.slice(1) : `-${this.current}`;
+				this.current = this.current.charAt(0) === '-' ? this.current.slice(1) : `-${this.current}`
 			}
 		},
 		percent() {
-			this.current = `${parseFloat(this.current) / 100.0}`;
+			this.current = `${parseFloat(this.current) / 100.0}`
 		},
 		append(number) {
 			if (this.operatorClicked) {
-				this.current = '';
-				this.operatorClicked = false;
+				this.current = ''
+				this.operatorClicked = false
 			}
-			this.current = `${this.current}${number}`;
-			this.equation.push(number);
+			this.current = `${this.current}${number}`
+			this.equation.push(number)
 		},
 		dot() {
 			if (this.current.indexOf('.') === -1) {
-				this.append('.');
+				this.append('.')
 			}
 		},
 		setPrevious(operator) {
-			this.previous = this.current;
-			this.operatorClicked = true;
-			this.equation.push(operator);
+			this.previous = this.current
+			this.operatorClicked = true
+			this.equation.push(operator)
 		},
 		divide() {
-			this.operator = (a, b) => b / a;
-			this.setPrevious('÷');
+			this.operator = (a, b) => b / a
+			this.setPrevious('÷')
 		},
 		multiply() {
-			this.operator = (a, b) => a * b;
-			this.setPrevious('×');
+			this.operator = (a, b) => a * b
+			this.setPrevious('×')
 		},
 		addition() {
-			this.operator = (a, b) => a + b;
-			this.setPrevious('+');
+			this.operator = (a, b) => a + b
+			this.setPrevious('+')
 		},
 		subtract() {
-			this.operator = (a, b) => a - b;
-			this.setPrevious('-');
+			this.operator = (a, b) => a - b
+			this.setPrevious('-')
 		},
 		equal() {
 			this.current = `${this.operator(
 				parseFloat(this.current),
 				parseFloat(this.previous),
-			)}`;
-			this.previous = null;
-			this.equation = [];
+			)}`
+			this.previous = null
+			this.equation = []
 		},
     }
   }
